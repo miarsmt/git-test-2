5b39e82f101c06bbb30f9f82c1f055f300487894
miarsmt <rusmiatim973@gmail.com>
2020-07-24 00:05:53 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta doloremque iste aut, consectetur accusamus eveniet maiores porro pariatur sed voluptate voluptatum quidem facere impedit blanditiis, rerum culpa, nesciunt quo commodi!</p>
 </body>
 </html>
